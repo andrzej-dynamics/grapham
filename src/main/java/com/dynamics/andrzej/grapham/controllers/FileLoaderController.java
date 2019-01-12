47827d871c8955845c08772f0849c2b83263ddc6
mijo <mijo@ailleron.com>
2019-01-13 00:14:03 +0100

    Dorzuciłem mapowanie do DTO w kontrolerze

--- src/main/java/com/dynamics/andrzej/grapham/controllers/FileLoaderController.java
+++ src/main/java/com/dynamics/andrzej/grapham/controllers/FileLoaderController.java
@@ -1,8 +1,15 @@
 package com.dynamics.andrzej.grapham.controllers;
 
+import com.dynamics.andrzej.grapham.Edge;
+import com.dynamics.andrzej.grapham.dtos.GraphDTO;
 import com.dynamics.andrzej.grapham.services.GraphLoader;
+import com.dynamics.andrzej.grapham.services.GraphService;
 import lombok.extern.slf4j.Slf4j;
+import org.jgrapht.Graph;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.DirectedAcyclicGraph;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,20 +23,24 @@ import java.io.IOException;
 @RestController
 public class FileLoaderController {
     private final GraphLoader graphLoader;
+    private final GraphService graphService;
 
-    public FileLoaderController(GraphLoader graphLoader) {
+    public FileLoaderController(GraphLoader graphLoader, GraphService graphService) {
         this.graphLoader = graphLoader;
+        this.graphService = graphService;
     }
 
-    @PostMapping("/file")
-    public ResponseEntity<String> loadFile(@RequestParam("file") MultipartFile file){
+    @PostMapping(value = "/file", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<GraphDTO> loadFile(@RequestParam("file") MultipartFile file){
         try {
             log.info("Got file to load graph: {}", file.getName());
-            graphLoader.loadGraph(file.getBytes());
-            return new ResponseEntity<>("Loaded", HttpStatus.OK);
+            final DirectedAcyclicGraph<Integer, Edge> graph = graphLoader.loadGraph(file.getBytes());
+            final GraphDTO dto = graphLoader.map(graph);
+            graphService.setGraph(graph);
+            return new ResponseEntity<>(dto, HttpStatus.OK);
         } catch (IllegalArgumentException | IOException e) {
             log.error("Cannot process graph", e);
-            return new ResponseEntity<>("Not loaded", HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
         }
     }
 }
