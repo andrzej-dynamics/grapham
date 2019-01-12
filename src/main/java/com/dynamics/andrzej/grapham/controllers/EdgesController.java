235c56411b0f9b4a241f7888e96c3ddad0e2cf34
mijo <mijo@ailleron.com>
2019-01-13 00:11:36 +0100

    Dodałem metody do kontrolerów, które posłużą do obsługi modyfikacji na grafie

--- src/main/java/com/dynamics/andrzej/grapham/controllers/EdgesController.java
+++ src/main/java/com/dynamics/andrzej/grapham/controllers/EdgesController.java
@@ -1,4 +1,21 @@
 package com.dynamics.andrzej.grapham.controllers;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class EdgesController {
+    public void canRemove(int source, int target) {
+
+    }
+    public void remove(int source, int target) {
+
+    }
+
+    public void canMove(int source, int target, int newTarget) {
+
+    }
+
+    public void move(int source, int target, int newTarget) {
+
+    }
 }
--- src/main/java/com/dynamics/andrzej/grapham/controllers/VerticesController.java
+++ src/main/java/com/dynamics/andrzej/grapham/controllers/VerticesController.java
@@ -1,4 +1,30 @@
 package com.dynamics.andrzej.grapham.controllers;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/vertices")
 public class VerticesController {
+    public void addVertex(int source, boolean isToSourceDirection) {
+
+    }
+
+    public void removeVertex(int vertex) {
+
+    }
+
+    public void switchSubgraphs(int source, int target) {
+
+    }
+
+    public void addEdge(int source, int target) {
+
+    }
+
+    public void canPerformVertexModification(int vertex) {
+
+    }
+
+    public void canPerformModificationBetweenVertices(int source, int target) {
+
+    }
 }
