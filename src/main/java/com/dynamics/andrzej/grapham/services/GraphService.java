17b94b2d1c323438d8ba6a0a5960bf869056d94d
mijo <mijo@ailleron.com>
2019-01-13 00:13:32 +0100

    Dodałem serwis do obsługi grafów wraz z testami

--- src/main/java/com/dynamics/andrzej/grapham/services/GraphService.java
+++ src/main/java/com/dynamics/andrzej/grapham/services/GraphService.java
@@ -1,17 +1,96 @@
 package com.dynamics.andrzej.grapham.services;
 
 import com.dynamics.andrzej.grapham.Edge;
+import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
-import org.jgrapht.Graph;
+import org.jgrapht.GraphPath;
+import org.jgrapht.alg.shortestpath.AllDirectedPaths;
+import org.jgrapht.graph.DirectedAcyclicGraph;
 import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Slf4j
 @Service
 public class GraphService {
-
     @Setter
-    private Graph<Integer, Edge> graph;
+    @Getter
+    private DirectedAcyclicGraph<Integer, Edge> graph;
 
+    public void addVertex(int source, boolean isToSourceDirection) {
+        if (graph == null || !graph.containsVertex(source)) {
+            throw new IllegalArgumentException("Graph not loaded or source does not exists!");
+        }
+        final int v = getNextVertexNumber();
+        graph.addVertex(v);
+        if (isToSourceDirection) {
+            graph.addEdge(v, source);
+        } else {
+            graph.addEdge(source, v);
+        }
+    }
 
+    public void removeVertex(int vertex) {
+        if (graph == null || !graph.containsVertex(vertex)) {
+            throw new IllegalArgumentException("Graph not loaded or source does not exists!");
+        }
+        if (graph.getDescendants(vertex).size() != 0) {
+            throw new IllegalArgumentException("Vertex has descendants, cannot be removed!");
+        }
+        graph.removeVertex(vertex);
+    }
+
+    public void addEdge(int source, int target) {
+        graph.addEdge(source, target);
+    }
+
+    public void switchSubgraphs(int source, int target) {
+        AllDirectedPaths<Integer, Edge> allDirectedPaths = new AllDirectedPaths<>(graph);
+        final List<GraphPath<Integer, Edge>> allPaths = allDirectedPaths.getAllPaths(source, target, false, Integer.MAX_VALUE);
+        if (!allPaths.isEmpty()) {
+            throw new IllegalArgumentException("Cannot perform switch, there is path between edges!");
+        }
+        final Set<Edge> incomingSource = new HashSet<>(graph.incomingEdgesOf(source));
+        final Set<Edge> outgoingSource = new HashSet<>(graph.outgoingEdgesOf(source));
+        final Set<Edge> incomingTarget = new HashSet<>(graph.incomingEdgesOf(target));
+        final Set<Edge> outgoingTarget = new HashSet<>(graph.outgoingEdgesOf(target));
+
+        graph.removeVertex(source);
+        graph.removeVertex(target);
+
+        graph.addVertex(source);
+        graph.addVertex(target);
+
+        addIncomingEdges(incomingSource, target);
+        addOutgoingEdges(outgoingSource, target);
+
+        addIncomingEdges(incomingTarget, source);
+        addOutgoingEdges(outgoingTarget, source);
+    }
+
+    private void addIncomingEdges(Set<Edge> incomingEdges, int vertex) {
+        for (Edge e : incomingEdges) {
+            graph.addEdge(e.source(), vertex);
+        }
+    }
+
+    private void addOutgoingEdges(Set<Edge> outgoingEdges, int vertex) {
+        for (Edge e : outgoingEdges) {
+            graph.addEdge(vertex, e.target());
+        }
+    }
+
+    private int getNextVertexNumber() {
+        final Set<Integer> vertices = graph.vertexSet();
+        int max = -1;
+        for (int v : vertices) {
+            if (v > max) {
+                max = v;
+            }
+        }
+        return max + 1;
+    }
 }
--- src/test/java/com/dynamics/andrzej/grapham/services/GraphServiceTest.java
+++ src/test/java/com/dynamics/andrzej/grapham/services/GraphServiceTest.java
@@ -1,5 +1,102 @@
+package com.dynamics.andrzej.grapham.services;
+
+import com.dynamics.andrzej.grapham.Edge;
+import org.jgrapht.Graph;
+import org.jgrapht.graph.DirectedAcyclicGraph;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import sun.security.jgss.GSSCaller;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import static org.testng.Assert.*;
 
 public class GraphServiceTest {
+    private GraphService graphService = new GraphService();
 
+    @BeforeMethod
+    public void setup() throws IOException, URISyntaxException {
+        final GraphLoader graphLoader = new GraphLoader();
+        final Path file = Paths.get(getClass().getClassLoader().getResource("graph.txt").toURI());
+        final DirectedAcyclicGraph<Integer, Edge> graph = graphLoader.loadGraph(Files.readAllBytes(file));
+        graphService.setGraph(graph);
+    }
+
+    @Test
+    public void testAddVertex() {
+        graphService.addVertex(9, true);
+        assertTrue(graphService.getGraph().vertexSet().contains(10));
+        assertNotNull(graphService.getGraph().getEdge(10, 9));
+    }
+
+    @Test
+    public void testRemoveVertex() {
+        graphService.removeVertex(9);
+        assertEquals(graphService.getGraph().vertexSet().size(), 9);
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testRemoveInvalidVertex() {
+        graphService.removeVertex(3);
+    }
+
+    @Test
+    public void testAddEdge() {
+        graphService.addEdge(8, 9);
+        assertNotNull(graphService.getGraph().getEdge(8, 9));
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testAddCyclicEdge() {
+        graphService.addEdge(9, 3);
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testAddNonexistentEdge() {
+        graphService.addEdge(9, 15);
+    }
+
+    @Test
+    public void testSwitchEdges() {
+        graphService.switchSubgraphs(4, 5);
+        assertNotNull(graphService.getGraph().getEdge(4, 8));
+        assertNotNull(graphService.getGraph().getEdge(5, 9));
+    }
+
+    @Test
+    public void testSwitchEdges2() {
+        graphService.switchSubgraphs(3, 2);
+        assertNotNull(graphService.getGraph().getEdge(2, 4));
+        assertNotNull(graphService.getGraph().getEdge(2, 5));
+        assertNotNull(graphService.getGraph().getEdge(2, 6));
+        assertNotNull(graphService.getGraph().getEdge(2, 7));
+        assertNotNull(graphService.getGraph().getEdge(2, 7));
+        assertNotNull(graphService.getGraph().getEdge(0, 2));
+        assertNotNull(graphService.getGraph().getEdge(3, 8));
+        assertNotNull(graphService.getGraph().getEdge(1, 3));
+
+        assertNull(graphService.getGraph().getEdge(1, 2));
+        assertNull(graphService.getGraph().getEdge(2, 8));
+        assertNull(graphService.getGraph().getEdge(0, 3));
+        assertNull(graphService.getGraph().getEdge(3, 4));
+        assertNull(graphService.getGraph().getEdge(3, 5));
+        assertNull(graphService.getGraph().getEdge(3, 6));
+        assertNull(graphService.getGraph().getEdge(3, 7));
+        assertNull(graphService.getGraph().getEdge(3, 7));
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testSwitchInvalidEdges() {
+        graphService.switchSubgraphs(3, 5);
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testSwitchInvalidEdges2() {
+        graphService.switchSubgraphs(3, 8);
+    }
 }
