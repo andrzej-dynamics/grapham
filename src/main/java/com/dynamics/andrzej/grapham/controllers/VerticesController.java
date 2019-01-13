3e562fefe86058e6f183d92152a0ceaf14628942
mijo <mijo@ailleron.com>
2019-01-13 01:21:02 +0100

    Dodałem metody w kontrolerze do obsługi wierzchołków

--- src/main/java/com/dynamics/andrzej/grapham/controllers/VerticesController.java
+++ src/main/java/com/dynamics/andrzej/grapham/controllers/VerticesController.java
@@ -1,30 +1,56 @@
 package com.dynamics.andrzej.grapham.controllers;
 
+import com.dynamics.andrzej.grapham.dtos.GraphDTO;
+import com.dynamics.andrzej.grapham.dtos.ModificationBetweenVerticesInfo;
+import com.dynamics.andrzej.grapham.dtos.ModificationVertexInfo;
+import com.dynamics.andrzej.grapham.services.GraphService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController("/vertices")
 public class VerticesController {
-    public void addVertex(int source, boolean isToSourceDirection) {
+    private final GraphService graphService;
 
+    public VerticesController(GraphService graphService) {
+        this.graphService = graphService;
     }
 
-    public void removeVertex(int vertex) {
-
+    @GetMapping("/add")
+    public GraphDTO addVertex(@RequestParam("source") int source, @RequestParam("direction") boolean isToSourceDirection) {
+        log.info("Adding vertex: {} direction: {}", source, isToSourceDirection);
+        return graphService.addVertex(source, isToSourceDirection);
     }
 
-    public void switchSubgraphs(int source, int target) {
-
+    @GetMapping("/remove")
+    public GraphDTO removeVertex(int vertex) {
+        log.info("Removing vertex: {}", vertex);
+        return graphService.removeVertex(vertex);
     }
 
-    public void addEdge(int source, int target) {
-
+    @GetMapping("/switch")
+    public GraphDTO switchSubgraphs(int source, int target) {
+        log.info("Switching vertex: {} to {}", source, target);
+        return graphService.switchSubgraphs(source, target);
     }
 
-    public void canPerformVertexModification(int vertex) {
-
+    @GetMapping("/add-edge")
+    public GraphDTO addEdge(int source, int target) {
+        log.info("Adding edge: {} to: {}", source, target);
+        return graphService.addEdge(source, target);
     }
 
-    public void canPerformModificationBetweenVertices(int source, int target) {
+    @GetMapping("/can-perform-single")
+    public ModificationVertexInfo canPerformVertexModification(int vertex) {
+        log.info("Checking whether can perform operations: {}", vertex);
+        return graphService.canPerformVertexModification(vertex);
+    }
 
+    @GetMapping("/can-perform-double")
+    public ModificationBetweenVerticesInfo canPerformModificationBetweenVertices(int source, int target) {
+        log.info("Checking whether can perform operations between: {} and: {}", source, target);
+        return graphService.canPerformModificationBetweenVertices(source, target);
     }
 }
