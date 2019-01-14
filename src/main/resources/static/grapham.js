006502f5d8c4332e75e3b68979bf6fc9136ba313
mijo <mijo@ailleron.com>
2019-01-14 12:18:08 +0100

    Wyświetlanie grafu i wstępna obsługa

--- src/main/resources/static/grapham.js
+++ src/main/resources/static/grapham.js
@@ -1,220 +1,233 @@
-$(document).ready(function () {
-    grapham();
-});
+// ---> Variables
+let nodes = [];
+let links = [];
 const radius = 15;
-const nodes_data = [
-    {"name": "Lillian", "sex": "F"},
-    {"name": "Gordon", "sex": "M"},
-    {"name": "Sylvester", "sex": "M"},
-    {"name": "Mary", "sex": "F"},
-    {"name": "Helen", "sex": "F"},
-    {"name": "Jamie", "sex": "M"},
-    {"name": "Jessie", "sex": "F"},
-    {"name": "Ashton", "sex": "M"},
-    {"name": "Duncan", "sex": "M"},
-    {"name": "Evette", "sex": "F"},
-    {"name": "Mauer", "sex": "M"},
-    {"name": "Fray", "sex": "F"},
-    {"name": "Duke", "sex": "M"},
-    {"name": "Baron", "sex": "M"},
-    {"name": "Infante", "sex": "M"},
-    {"name": "Percy", "sex": "M"},
-    {"name": "Cynthia", "sex": "F"},
-    {"name": "Feyton", "sex": "M"},
-    {"name": "Lesley", "sex": "F"},
-    {"name": "Yvette", "sex": "F"},
-    {"name": "Maria", "sex": "F"},
-    {"name": "Lexy", "sex": "F"},
-    {"name": "Peter", "sex": "M"},
-    {"name": "Ashley", "sex": "F"},
-    {"name": "Finkler", "sex": "M"},
-    {"name": "Damo", "sex": "M"},
-    {"name": "Imogen", "sex": "F"}
-];
-const links_data = [
-    {"source": "Sylvester", "target": "Gordon", "type": "A"},
-    {"source": "Sylvester", "target": "Lillian", "type": "A"},
-    {"source": "Sylvester", "target": "Mary", "type": "A"},
-    {"source": "Sylvester", "target": "Jamie", "type": "A"},
-    {"source": "Sylvester", "target": "Jessie", "type": "A"},
-    {"source": "Sylvester", "target": "Helen", "type": "A"},
-    {"source": "Helen", "target": "Gordon", "type": "A"},
-    {"source": "Mary", "target": "Lillian", "type": "A"},
-    {"source": "Ashton", "target": "Mary", "type": "A"},
-    {"source": "Duncan", "target": "Jamie", "type": "A"},
-    {"source": "Gordon", "target": "Jessie", "type": "A"},
-    {"source": "Sylvester", "target": "Fray", "type": "E"},
-    {"source": "Fray", "target": "Mauer", "type": "A"},
-    {"source": "Fray", "target": "Cynthia", "type": "A"},
-    {"source": "Fray", "target": "Percy", "type": "A"},
-    {"source": "Percy", "target": "Cynthia", "type": "A"},
-    {"source": "Infante", "target": "Duke", "type": "A"},
-    {"source": "Duke", "target": "Gordon", "type": "A"},
-    {"source": "Duke", "target": "Sylvester", "type": "A"},
-    {"source": "Baron", "target": "Duke", "type": "A"},
-    {"source": "Baron", "target": "Sylvester", "type": "E"},
-    {"source": "Evette", "target": "Sylvester", "type": "E"},
-    {"source": "Cynthia", "target": "Sylvester", "type": "E"},
-    {"source": "Cynthia", "target": "Jamie", "type": "E"},
-    {"source": "Mauer", "target": "Jessie", "type": "E"},
-    {"source": "Duke", "target": "Lexy", "type": "A"},
-    {"source": "Feyton", "target": "Lexy", "type": "A"},
-    {"source": "Maria", "target": "Feyton", "type": "A"},
-    {"source": "Baron", "target": "Yvette", "type": "E"},
-    {"source": "Evette", "target": "Maria", "type": "E"},
-    {"source": "Cynthia", "target": "Yvette", "type": "E"},
-    {"source": "Maria", "target": "Jamie", "type": "E"},
-    {"source": "Maria", "target": "Lesley", "type": "E"},
-    {"source": "Ashley", "target": "Damo", "type": "A"},
-    {"source": "Damo", "target": "Lexy", "type": "A"},
-    {"source": "Maria", "target": "Feyton", "type": "A"},
-    {"source": "Finkler", "target": "Ashley", "type": "E"},
-    {"source": "Sylvester", "target": "Maria", "type": "E"},
-    {"source": "Peter", "target": "Finkler", "type": "E"},
-    {"source": "Ashley", "target": "Gordon", "type": "E"},
-    {"source": "Maria", "target": "Imogen", "type": "E"}
-
-];
 let node;
 let g;
+let circle;
 let link;
 let simulation;
+let firstSelectedNode;
+let secondSelectedNode;
+let selectedEdge;
+// ---> JQuery Handlers
+$(document).ready(function () {
+    $('#customFileLang').change(onChangeFileInput);
+});
 
-function grapham() {
+function onChangeFileInput() {
+    const file = this.files[0];
+    const formData = new FormData();
+    formData.append('file', file, file.name);
+    $('.custom-file-label').html(file.name);
+    upload(formData, onLoadGraphSuccess, null);
+}
+
+function onLoadGraphSuccess(data) {
+    console.log(data);
+    nodes = data.v.map(v => ({name: `v${v}`}));
+    links = data.e.map(e => ({source: `v${e.s}`, target: `v${e.t}`}));
+    resetValues();
+    loadGraph();
+}
+
+function resetValues() {
+    d3.selectAll("svg > g").remove();
+    node = undefined;
+    g = undefined;
+    circle = undefined;
+    link = undefined;
+    simulation = undefined;
+    firstSelectedNode = undefined;
+    secondSelectedNode = undefined;
+}
+
+function showManipulationOptions() {
+    let singleSelectedNodeOptions = $('#singleSelectedNodeOptions');
+    let twoSelectedNodesOptions = $('#twoSelectedNodesOptions');
+    let edgeOptions = $('#edgeOptions');
+    let switchEdgeOptions = $('#switchEdgeOptions');
+
+    singleSelectedNodeOptions.attr('hidden', 'hidden');
+    twoSelectedNodesOptions.attr('hidden', 'hidden');
+    edgeOptions.attr('hidden', 'hidden');
+    switchEdgeOptions.attr('hidden', 'hidden');
+
+    if (firstSelectedNode && !secondSelectedNode && !selectedEdge) {
+        singleSelectedNodeOptions.removeAttr('hidden');
+    } else if (!firstSelectedNode && secondSelectedNode && !selectedEdge) {
+        singleSelectedNodeOptions.removeAttr('hidden');
+    } else if (firstSelectedNode && secondSelectedNode && !selectedEdge) {
+        twoSelectedNodesOptions.removeAttr('hidden');
+    } else if (!firstSelectedNode && !secondSelectedNode && selectedEdge) {
+        edgeOptions.removeAttr('hidden');
+    } else if (firstSelectedNode && secondSelectedNode && selectedEdge) {
+        switchEdgeOptions.removeAttr('hidden');
+    }
+}
+
+// ---> Graph
+function loadGraph() {
     let svg = d3.select("svg");
     let width = +svg.attr("width");
     let height = +svg.attr("height");
     //set up the simulation and add forces
-    simulation = d3.forceSimulation().nodes(nodes_data);
-
-    let link_force = d3.forceLink(links_data)
-        .id(function (d) {
-            return d.name;
-        });
-
-    let charge_force = d3.forceManyBody()
-        .strength(-100);
-
-    const center_force = d3.forceCenter(width / 2, height / 2);
+    simulation = d3.forceSimulation().nodes(nodes);
+    let linkForce = d3.forceLink(links).id((d) => (d.name)).distance(100).strength(1);
+    let chargeForce = d3.forceManyBody().strength(-500);
+    const centerForce = d3.forceCenter(width / 2, height / 2);
 
 //add encompassing group for the zoom
-    g = svg.append("g")
-        .attr("class", "everything");
+    g = svg.append("g").attr("class", "everything");
 
 //draw lines for the links
     link = g.append("g")
         .attr("class", "links")
         .selectAll("line")
-        .data(links_data)
-        .enter().append("line")
+        .data(links)
+        .enter()
+        .append("line")
         .attr("stroke-width", 4)
-        .style("stroke", linkColour);
+        .attr("marker-end", "url(#arrow)")
+        .style("stroke", '#eae110')
+        .on("click", onClickEdge)
+        .on("mouseover", onEdgeMouseOver)
+        .on("mouseout", onEdgeMouseOut);
+
 
 //draw circles for the nodes
     node = g.append("g")
         .attr("class", "nodes")
-        .selectAll("circle")
-        .data(nodes_data)
+        .selectAll("g")
+        .data(nodes)
         .enter()
-        .append("circle")
+        .append("g");
+
+
+    circle = node.append("circle")
         .attr("r", radius)
-        .attr("fill", circleColour)
-        .on("mouseover", handleMouseOver)
-        .on("mouseout", handleMouseOut);
+        .attr("fill", '#597db1')
+        .on("click", onClickNode)
+        .on("mouseover", onNodeMouseOver)
+        .on("mouseout", onNodeMouseOut);
 
 
-//add drag capabilities
-    var drag_handler = d3.drag()
-        .on("start", drag_start)
-        .on("drag", drag_drag)
-        .on("end", drag_end);
+    node.append("text")
+        .attr("class", "infoText")
+        .attr("dx", 15)
+        .attr("dy", 4)
+        .text(function (d) {
+            return d.name
+        });
 
-    drag_handler(node);
-
+    const dragHandler = d3.drag()
+        .on("start", dragStart)
+        .on("drag", dragDrag)
+        .on("end", dragEnd);
+    dragHandler(node);
 
 //add zoom capabilities
-    var zoom_handler = d3.zoom()
-        .on("zoom", zoom_actions);
+    const zoomHandler = d3.zoom().on("zoom", zoomActions);
     simulation
-        .force("charge_force", charge_force)
-        .force("center_force", center_force)
-        .force("links", link_force)
-    ;
+        .force("charge_force", chargeForce)
+        .force("center_force", centerForce)
+        .force("links", linkForce);
     //add tick instructions:
     simulation.on("tick", tickActions);
-    zoom_handler(svg);
+    zoomHandler(svg);
 }
 
-function circleColour(d) {
-    d.colour = "#597db1";
-    return "#597db1";
+function onClickNode(selectedNode, index) {
+    console.log(selectedNode);
+    let selectionChanged = false;
+    if (firstSelectedNode === selectedNode) {
+        firstSelectedNode = undefined;
+        d3.select(this).attr("fill", '#597db1');
+        selectionChanged = true;
+    } else if (secondSelectedNode === selectedNode) {
+        secondSelectedNode = undefined;
+        d3.select(this).attr("fill", '#597db1');
+        selectionChanged = true;
+    } else if (!firstSelectedNode) {
+        firstSelectedNode = selectedNode;
+        d3.select(this).attr("fill", '#4b1515');
+        selectionChanged = true;
+    } else if (!secondSelectedNode) {
+        secondSelectedNode = selectedNode;
+        d3.select(this).attr("fill", '#4b1515');
+        selectionChanged = true;
+    }
+
+    if (selectionChanged) {
+        showManipulationOptions();
+    }
 }
 
-function linkColour(d) {
-    return "#eae110";
+function onNodeMouseOver(selectedNode, i) {
+    if (selectedNode !== firstSelectedNode && selectedNode !== secondSelectedNode) {
+        d3.select(this).attr("fill", '#86b3ff');
+    }
 }
 
-function handleMouseOver(d, i) {
-    console.log(d);
-    d.colour = "#86b3ff";
-}
-function handleMouseOut(d, i) {
-    // d3.select(this).attr({
-    //     fill: "#597db1"
-    // });
-
+function onNodeMouseOut(selectedNode, i) {
+    if (selectedNode !== firstSelectedNode && selectedNode !== secondSelectedNode) {
+        d3.select(this).attr("fill", '#597db1');
+    }
 }
 
-//Drag functions
-//d is the node
-function drag_start(d) {
+function onClickEdge(edge, index) {
+    console.log(edge);
+    if (selectedEdge === edge) {
+        selectedEdge = undefined;
+        d3.select(this).attr("fill", '#f9ff0b');
+        showManipulationOptions();
+    } else if (!selectedEdge) {
+        selectedEdge = edge;
+        d3.select(this).attr("fill", '#4b1515');
+        showManipulationOptions();
+    }
+}
+
+function onEdgeMouseOver(edge, i) {
+    if (edge !== firstSelectedNode) {
+        d3.select(this).attr("fill", '#f9ff0b');
+    }
+}
+
+function onEdgeMouseOut(edge, i) {
+    if (edge !== firstSelectedNode) {
+        d3.select(this).attr("fill", '#eae110');
+    }
+}
+
+function dragStart(d) {
     if (!d3.event.active) simulation.alphaTarget(0.3).restart();
     d.fx = d.x;
     d.fy = d.y;
 }
 
 //make sure you can't drag the circle outside the box
-function drag_drag(d) {
+function dragDrag(d) {
     d.fx = d3.event.x;
     d.fy = d3.event.y;
 }
 
-function drag_end(d) {
+function dragEnd(d) {
     if (!d3.event.active) simulation.alphaTarget(0);
     d.fx = null;
     d.fy = null;
 }
 
 //Zoom functions
-function zoom_actions() {
+function zoomActions() {
     g.attr("transform", d3.event.transform)
 }
 
 function tickActions() {
-    //update circle positions each tick of the simulation
-    node
-        .attr("cx", function (d) {
-            return d.x;
-        })
-        .attr("cy", function (d) {
-            return d.y;
-        })
-        .attr("fill", function (d) {
-            return d.colour;
-        });
-
-    //update link positions
+    node.attr("transform", (d) => "translate(" + d.x + "," + d.y + ")");
     link
-        .attr("x1", function (d) {
-            return d.source.x;
-        })
-        .attr("y1", function (d) {
-            return d.source.y;
-        })
-        .attr("x2", function (d) {
-            return d.target.x;
-        })
-        .attr("y2", function (d) {
-            return d.target.y;
-        });
+        .attr("x1", (d) => d.source.x)
+        .attr("y1", (d) => d.source.y)
+        .attr("x2", (d) => d.target.x)
+        .attr("y2", (d) => d.target.y);
 }
